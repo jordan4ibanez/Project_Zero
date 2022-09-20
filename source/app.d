c5683ef5699f664f06c635d6be6d95b59f3efad1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-20 02:26:31 -0400

    Create a basic freecam

--- source/app.d
+++ source/app.d
@@ -61,6 +61,39 @@ void main()
 
     while(!WindowShouldClose()) {
 
+
+        /// Freecam test
+        if (keyboard.getForward()) {
+            Vector3 direction = camera3d.getForward();
+            Vector3 position = camera3d.getPosition();
+            camera3d.setPosition(Vector3Add(position, direction));
+        } else if (keyboard.getBack()) {
+            Vector3 direction = camera3d.getForward();
+            Vector3 position = camera3d.getPosition();
+            direction = Vector3Multiply(direction, Vector3(-1,-1,-1));
+            camera3d.setPosition(Vector3Add(position, direction));
+        }
+        if (keyboard.getRight()) {
+            Vector3 direction = camera3d.getRight();
+            Vector3 position = camera3d.getPosition();
+            camera3d.setPosition(Vector3Add(position, direction));
+        } else if (keyboard.getLeft()) {
+            Vector3 direction = camera3d.getRight();
+            Vector3 position = camera3d.getPosition();
+            direction = Vector3Multiply(direction, Vector3(-1,-1,-1));
+            camera3d.setPosition(Vector3Add(position, direction));
+        }
+        if (keyboard.getJump()) {
+            Vector3 direction = camera3d.getUp();
+            Vector3 position = camera3d.getPosition();
+            camera3d.setPosition(Vector3Add(position, direction));
+        } else if (keyboard.getRun()) {
+            Vector3 direction = camera3d.getUp();
+            Vector3 position = camera3d.getPosition();
+            direction = Vector3Multiply(direction, Vector3(-1,-1,-1));
+            camera3d.setPosition(Vector3Add(position, direction));
+        }
+
         deltaCalculator.calculateDelta();
 
         double delta = deltaCalculator.getDelta();
