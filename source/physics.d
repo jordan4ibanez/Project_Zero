1da2f28da5afd512c4af1b47a4e22bb3b0bfd6ad
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-27 21:46:13 -0400

    Begin AABB collision detection implementation

--- source/physics.d
+++ source/physics.d
@@ -1,15 +1,10 @@
 module physics;
 
 import std.stdio;
-import dmech.geometry;
-import dmech.rigidbody;
-import dmech.world;
-import dlib.core.memory;
-import dlib.math.vector;
-import dlib.math.matrix;
 import raylib;
+import std.uuid;
 
-/// This class is a wrapper for bindbc newton physics
+/// This is an extremely basic physics engine that uses AABB physics to work
 public class PhysicsEngine {
 
     PhysicsWorld world;
@@ -19,29 +14,12 @@ public class PhysicsEngine {
     immutable double fpsPrecision = 300;
     immutable double lockedTick = 1.0 / this.fpsPrecision;
 
+
+    
+
+
     this() {
-        this.world = New!PhysicsWorld(null);
-    }
 
-    ~this() {
-        Delete(this.world);
-    }
-
-    RigidBody addGround() {
-        RigidBody bGround = this.world.addStaticBody(Vector3f(0,-1,0));
-        Geometry ground = New!GeomBox(this.world, Vector3f(40, 1, 40));
-        this.world.addShapeComponent(bGround, ground, Vector3f(0,0,0), 1);
-
-        return bGround;
-    }
-
-    RigidBody addBody() {
-        // bSphere - dynamic sphere with radius of 1 m and mass of 1 kg 
-        RigidBody bSphere = world.addDynamicBody(Vector3f(-4.0f, 20.0f, 0.0f), 0.0f);
-        Geometry gSphere = New!GeomSphere(world, 1.0f);
-        world.addShapeComponent(bSphere, gSphere, Vector3f(0.0f, 0.0f, 0.0f), 1.0f);
-
-        return bSphere;
     }
 
     double getTimeAccumulator() {
@@ -58,21 +36,15 @@ public class PhysicsEngine {
 
     /// Remember: this needs an external handler for fixed time stamps!
     void update() {
-        world.update(this.lockedTick);
-    }
-    
-    auto get() {
-        return world.shapeComponents;
+
     }
 
 }
 
 
-
-
-
-
-
-
-
-
+/// Entities are 3D boxes that 
+public class Entity {
+    Vector3 position;
+    Vector3 velocity;
+    float rotation;
+}
