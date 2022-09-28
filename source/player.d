880374f816f516d2f1ba86891204a9d7a6c18fc5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-27 22:58:16 -0400

    Remove dlib

--- source/player.d
+++ source/player.d
@@ -2,13 +2,10 @@ module player;
 
 import raylib;
 import std.stdio;
-import dmech.rigidbody;
-import dlib.math.vector;
 
 
 public class Player {
 
-    RigidBody playerBody;
 
     this(Vector3 position) {
         
