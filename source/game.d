33513097fd4575986da6565912dd126d2df9297c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-30 21:28:33 -0400

    Implement tests, render() function

--- source/game.d
+++ source/game.d
@@ -12,6 +12,10 @@ import window;
 import raylib;
 import world;
 
+
+/// This needs to be moved into it's own object
+import fast_noise;
+
 public class Game {
 
     Window window;
@@ -39,8 +43,36 @@ public class Game {
         timeKeeper  = new TimeKeeper(this);
         world       = new World(this);
 
+
+        /// Temporary debugging things
         mouse.grab();
 
+        for (int i = 0; i < 10; i++) {
+            Entity myNewEntity = new Entity(Vector3(50,i * 2, 50), Vector3(1,1,1), Vector3(0,0,0));
+            world.addEntity(myNewEntity);
+            writeln(myNewEntity.getUUID());
+            // boxes ~= physicsEngine.addBox(Vector3(3, 1 + i * 10,0));
+
+        }
+
+        float[] heightMap;
+
+        /// testing out a random map!
+
+        int size = 250;
+
+        FNLState noiseEngine = fnlCreateState(1234);
+        noiseEngine.noise_type = FNLNoiseType.FNL_NOISE_OPENSIMPLEX2;
+        noiseEngine.frequency = 0.01;
+        noiseEngine.octaves = 10;
+
+        for (int x = 0; x < size; x++) {
+            for (int z = 0; z < size; z++) {
+                heightMap ~= fnlGetNoise2D(&noiseEngine, x, z) * 50;
+            }
+        }
+
+        world.uploadHeightMap(heightMap, 10);
 
 
 
@@ -52,8 +84,23 @@ public class Game {
     }
 
     void run() {
-        writeln("I'm running!");
+        while (!window.shouldClose()) {
+
+            timeKeeper.calculateDelta();
+            window.update();
+            mouse.update();
+            keyboard.update();
+            camera3d.update();            
+
+            this.render();
+
+        }
     }
 
+    void render() {
+        BeginDrawing();
+
+        EndDrawing();
+    }
 
 }
