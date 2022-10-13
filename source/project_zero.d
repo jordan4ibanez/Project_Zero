4fdda24a39435b9eda2ca9e81ba719325bc42088
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-10-12 22:05:10 -0400

    Fix unneeded scope, module, & import

--- source/project_zero.d
+++ source/project_zero.d
@@ -1,15 +1,10 @@
-module project_zero;
-
 import game.game;
-import raylib;
 
 void main() {
 
-    {
-        Game game = new Game();
-        game.run();
-        game.cleanUp();
-    }
+    Game game = new Game();
+    game.run();
+    game.cleanUp();
 
     // Game cleans itself up
 }
